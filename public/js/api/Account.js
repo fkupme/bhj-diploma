2b23f3334c474d232543389318e48cb0ccbf7305
V. Kirichenko <fkupme@yandex.ru>
2024-05-21 13:40:43 +0300

    have problem in createRequest

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -4,10 +4,15 @@
  * Имеет свойство URL со значением '/account'
  * */
 class Account extends Entity {
+	constructor(){
+		super();
+	}
+	static URL = '/account';
   /**
    * Получает информацию о счёте
    * */
+  
   static get(id = '', callback){
-
+	createRequest({url: `${this.URL}/${id}`, ...data, method: 'GET', callback});
   }
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,72 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+	const xhr = new XMLHttpRequest();
+
+	const { url, data, method, callback } = options;
+
+	if (!url || !data || !method || !callback) {
+		throw new Error(`Invalid request`);
+	}
+
+	const dataEnteries = Object.entries(data);
+
+	xhr.responseType = 'json';
+	xhr.addEventListener('load', (e) => {
+		if (xhr.status === 200) {
+			callback(xhr.response);
+		}
+	});
+
+	if (method === 'GET') {
+		let getURL = url + '?';
+
+		dataEnteries.forEach((pair, index) => {
+			const stringPair = `${pair[0]}=${pair[1]}`
+			if (index === 0) {
+				getURL += stringPair
+			} else {
+				getURL += '&' + stringPair
+			}
+		})
+
+		try {
+			xhr.open(method, getURL);
+			xhr.send();
+		} catch (error) {
+			console.error(error)
+		}
+
+	} else {
+		const formData = new FormData;
+
+		dataEnteries.forEach(pair => formData.append(...pair))
+
+		try {
+			xhr.open(method, url);
+			xhr.send(formData);
+		} catch (error) {
+			console.error(error)
+		}
+
+	}
 
 };
+
+createRequest({
+	url: 'http://localhost:8000',
+	data: { email: "demo@demo", password: "demo"},
+	method: 'GET',
+	callback: (response) => {
+		console.log(response);
+	}
+});
+
+createRequest({
+	url: 'http://localhost:8000',
+	data: { email: "demo@demo", password: "demo"},
+	method: 'POST',
+	callback: (response) => {
+		console.log(response);
+	}
+});
